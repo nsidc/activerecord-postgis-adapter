aaebb9b24c9a17d7ceaa461b4c532cc941663ea6
billinb <billinb@nsidc.org>
2013-08-26 09:31:20 -0600

    Trying to get gem working with jruby again.

--- lib/active_record/connection_adapters/postgis_adapter.rb
+++ lib/active_record/connection_adapters/postgis_adapter.rb
@@ -60,8 +60,13 @@ end
 # :stopdoc:
 
 require 'active_record'
-require 'active_record/connection_adapters/postgresql_adapter'
 require 'rgeo/active_record'
+
+if defined?(::RUBY_ENGINE) && ::RUBY_ENGINE == 'jruby'
+  require 'active_record/connection_adapters/postgis_adapter/jdbc_connection'
+else
+  require 'active_record/connection_adapters/postgresql_adapter'
+end
 
 case ::ActiveRecord::VERSION::MAJOR
 when 3
